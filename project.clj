46a46edd8d49e2e193430cd95f74d09a8191c6a0
Gary Fredericks <fredericksgary@gmail.com>
2015-11-18 16:21:58 -0600

    Set deploy repositories

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/gfredericks/lein-lein"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :deploy-repositories [["releases" :clojars]])

abf4df32ea33c10354180861943954c7987f728d
Gary Fredericks <fredericksgary@gmail.com>
2015-11-18 16:11:46 -0600

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-lein "0.2.0-SNAPSHOT"
-  :description "A Leiningen plugin for running Leiningen tasks."
+  :description "A collection of Leiningen plugins for doing all sorts of useful things"
   :url "https://github.com/gfredericks/lein-lein"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
